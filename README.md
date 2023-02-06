73b6ec660022bcd6c4ac9b0c25116a7c923df70f
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2023-02-06 13:02:11 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# contact-list
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-qsskdf)
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import "./index.css";
+import App from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+
+)
